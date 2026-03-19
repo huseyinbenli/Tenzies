40a916216c45e9873c240b09867688bd9e9af7da
huseyinbenli <huseyinbenli@outlook.fr>
2026-03-19 16:06:08 +0100

    accessibility improved

--- src/Die.jsx
+++ src/Die.jsx
@@ -3,7 +3,13 @@ export default function Die(props) {
     backgroundColor: props.isHeld ? "#59E391" : "white",
   };
   return (
-    <button style={style} onClick={props.handleClick}>
+    <button
+      style={style}
+      onClick={props.handleClick}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+    >
       {props.value}
     </button>
   );
