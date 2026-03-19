2b2e1c0366609e70bc1f8cb1ad02cc09631ab818
huseyinbenli <huseyinbenli@outlook.fr>
2026-03-19 15:09:14 +0100

    conditional bg color function passed down as props

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,3 +1,10 @@
 export default function Die(props) {
-  return <button>{props.value}</button>;
+  const style = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+  return (
+    <button style={style} onClick={props.handleClick}>
+      {props.value}
+    </button>
+  );
 }
